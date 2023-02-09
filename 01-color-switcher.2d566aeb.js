dd983d105639dc16c82a05caf0314ab62b03f828
Alex-Fedkov <107791953+Alex-Fedkov@users.noreply.github.com>
2023-02-09 12:58:19 +0000

    Deploying to gh-pages from @ Alex-Fedkov/goit-js-hw-09@827c9a8890fcfc6bddd609cee16fc6d78ac7d291 🚀

--- 01-color-switcher.2d566aeb.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");e.addEventListener("click",(()=>{e.setAttribute("disabled",!0),timerId=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=e}),1e3)})),r.addEventListener("click",(()=>{clearInterval(timerId),e.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.2d566aeb.js.map
