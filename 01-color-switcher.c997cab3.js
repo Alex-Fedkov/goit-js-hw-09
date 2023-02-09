c8dc853832f94f19ea8b61150c375c26def000f7
Alex-Fedkov <107791953+Alex-Fedkov@users.noreply.github.com>
2023-02-09 13:23:36 +0000

    Deploying to gh-pages from @ Alex-Fedkov/goit-js-hw-09@3921ec03f7755ad6795599b2f12fae1f7f81f1ac 🚀

---
+++ 01-color-switcher.c997cab3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");let o=null;e.addEventListener("click",(()=>{e.setAttribute("disabled",!0),o=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=e}),1e3)})),r.addEventListener("click",(()=>{clearInterval(o),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.c997cab3.js.map
